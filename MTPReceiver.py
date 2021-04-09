3f728d21e9371a3bfd7f177d2532ce80e715ee1b
laurastreet <70777920+laurastreet@users.noreply.github.com>
2021-04-08 22:53:22 -0400

    Add files via upload

--- MTPReceiver.py
+++ MTPReceiver.py
@@ -4,31 +4,101 @@
 ## import (add more if you need)
 import unreliable_channel
 from socket import *
+import sys
+import time
+import zlib
+DATA = 0
+ACK = 1
 
-def create_packet():
-	print('do sth here')
-# Two types of packets, data and ack
-    # ACK packet header has the following fields - type, seqNum, length, checksum
-# crc32 available through zlib library
+#ACK packets have the same fields as the data packets, without any data
+def create_packet(seq_num):
+	packet_type = 1
+
+	length = 0		#ACK packets do not contain data
+	bytes_arr = bytes(str(packet_type) + str(seq_num) + str(length),'utf-8')
+	checksum = zlib.crc32(bytes_arr)
+	checksum = str(checksum)
+	checksum = bytes(checksum, 'utf-8')
+	packet = bytes_arr + checksum
+	return packet
 
 
-def extract_packet_info():
-	print('do sth here')
+#should return the sequence number of packet and whether it is corrupt or not
+#note that the checksum length usually seems to equal 10, but sometimes it also equals 9 or 8
+def extract_packet_info(ModifiedMessage):
+	print('sys.getsizeof(ModifiedMessage): ', sys.getsizeof(ModifiedMessage))		#between 1470-1472 bytes
+	print('ModifiedMessage: ', ModifiedMessage)
+	decoded_packet = ModifiedMessage.decode('utf-8')
+	print('decoded_packet: ', decoded_packet)
+	end_numSeq = decoded_packet.find('1400')
+	print('end_numSeq')
+
+	packet_type = ModifiedMessage[0:1]
+	seq_num = ModifiedMessage[1:end_numSeq]		#this is sandwiched in between packet_type and length (will vary but Receiver will
+		#eventually know what to expect) - keep at length 1 for now
+	print('seq_num: ', seq_num)
+	length = 1400 									#this is fixed
+
+	bytes_arr = bytes(decoded_packet[0:-10], 'utf-8')
+	checksum = decoded_packet[-10:]
+	if(checksum[0]=='\''):
+		checksum = checksum[1:]
+		bytes_arr = bytes(decoded_packet[0:-9], 'utf-8')
+	checksum = int(checksum)
+	print('checksum_sender: ', checksum)
+	corrupt = check_for_corruption(bytes_arr, checksum)
+	print('corrupt: ', corrupt)
+	return seq_num, corrupt
 # extract the packet data after receiving
 
+def check_for_corruption(bytes_arr, checksum_sender):
+	checksum = zlib.crc32(bytes_arr)				#returns an integer
+	print('checksum: ', checksum)
+	if(checksum == checksum_sender):
+		return False
+	else:
+		return True
 
-def main():
+def MTPReceiver_main(arg):
+	print('MTPReceiver starting')
+	expected_seqNum = 0
+	ACK = 'ACK' + str(expected_seqNum)
 	# read the command line arguments
+	# receiver_port = sys.argv[1]
+	# output_file = sys.argv[2]
+	# log_filename = sys.argv[3]
 
 	# open log file and start logging
+	# logfile = open(log_filename, "a")
 
-	# open server socket and bind - 'receiver opens up a UDP server socket'
+	# open server socket and bind
+	receiverAddr = '192.168.1.14'
+	serverAddr = '192.168.1.15'
+	receiverPort = 49152
+	serverPort = 49153  # other port can be 631
+	receiverSocket = socket(AF_INET, SOCK_DGRAM)  # AF_INET indicates that the underlying network is using IPv4
+		# SOCK_DGRAM indicates that socket is a UDP socket
+		# the OS creates the port number of the client socket (per textbook)
+	receiverSocket.bind((receiverAddr,receiverPort))
+	print('socket bound - MTPReceiver')
+	receiverSocket.settimeout(5)  # 5s timeout (change to 500ms later)
+	sendingSocket = socket(AF_INET, SOCK_DGRAM)
 
-	while True:
+
+	while(True):
 		# receive packet, but using our unreliable channel
-
+		# received_data, recv_addr = unreliable_channel.recv_packet(clientSocket)  # last argument is an open port
+		modifiedMessage, serverAddress = receiverSocket.recvfrom(2048)  # modifiedMessage contains packet data - 2048 is the buffer size
+		# print('serverAddress: ', serverAddress)
+		time.sleep(0.2)
 		# call extract_packet_info
+		seq_num, corrupt = extract_packet_info(modifiedMessage)
+		if(seq_num==expected_seqNum and not corrupt):
+			# check for corruption and lost packets, send ack accordingly
+			ACK = create_packet(seq_num)
+			sendingSocket.sendto(ACK, (serverAddr, serverPort))
+			expected_seqNum +=1
+			# ACK = 'ACK' + str(expected_seqNum)
 
-		# check for corruption and lost packets, send ack accordingly
-
+MTPReceiver_main(0)
 
--- MTPSender.py
+++ MTPSender.py
@@ -5,10 +5,14 @@
 ## import (add more if you need)
 import threading
 import sys
+import logging
 import unreliable_channel
 import zlib
+import time
 from socket import *
 
+DATA = 0
+ACK = 1
 
 ## define and initialize
 # window_size, window_base, next_seq_number, dup_ack_count, etc.
@@ -17,7 +21,8 @@ from socket import *
 
 
 ## we will need a lock to protect against concurrent threads
-# lock = threading.Lock()
+#NOTE - you cannot receive and send at the same time
+lock = threading.Lock()
 
 # take the input file and split it into packets (use create_packet)
 # MTU = 1500bytes
@@ -26,43 +31,86 @@ from socket import *
     #UDP header = 8 bytes
     #IP header = 20 bytes
 def create_packet(data, seqNum):
-# Two types of packets, data and ack
-    #MTU header has the following fields - type, seqNum, length, checksum (4 bytes)
-    #MTP header + data gets encapsulated in UDP header (UDP header gets added automatically by the socket -> don't need to implement)
-# crc32 available through zlib library
     #after adding the first 3 fields of MTP data and header, sender calculates a 32-bit checksum and appends it to the packet
-
-
-    # checksum = zlib.crc32()
-    print('data: ', data, ', seqNum: ', seqNum)
     packet = ''
+    packet_type = DATA
+    length = len(data)
+    # print('length: ', length)
+
+    #print('data: ', data, ', seqNum: ', seqNum)
+    bytes_arr = bytes(str(packet_type) + str(seqNum) + str(length) + str(data), 'utf-8')
+    # print('sizeof(bytes_arr): ', sys.getsizeof(bytes_arr))
+    checksum = zlib.crc32(bytes_arr)
+    checksum = str(checksum)
+    checksum = bytes(checksum, 'utf-8')
+    # print('sizeof(checksum): ', sys.getsizeof(checksum))
+    # print(type(checksum))
+    packet = bytes_arr + checksum
+    # print('checksum:', checksum)
+
     return packet
 
-def extract_packet_info():
-    print("do sth here")
+def extract_packet_info(packet_from_server):
+    decoded_packet = packet_from_server.decode('utf-8')
+    packet_type = decoded_packet[0:1]
+    # seq_num = decoded_packet[1:-2]    ###unsure how to find this, since we don't have length...
+    # len = decoded_packet[]
+    # checksum = decoded_packet[]
+    bytes_arr = []
+    corrupt = check_for_corruption(bytes_arr,checksum)
 # extract the packet data after receiving
+    return seq_num,corrupt
 
+def check_for_corruption(bytes_arr,checksum_sender):
+    checksum = zlib.crc32(bytes_arr)
+    if(checksum==checksum_sender):
+        return False
+    else:
+        return True
 
-def receive_thread():
-    print('do sth here')
-	# while True:
-		# receive packet, but using our unreliable channel
-		# packet_from_server, server_addr = unreliable_channel.recv_packet(socket)
-		# call extract_packet_info
+def receive_thread(socket):
+    while(True):
+        # receive packet, but using our unreliable channel
+		packet_from_server, server_addr = unreliable_channel.recv_packet(socket)
+        extract_packet_info(packet_from_server)
+        # packet = extract_packet_info(packet_from_server)
 		# check for corruption, take steps accordingly
 		# update window size, timer, triple dup acks
 
 
-def main():
+def MTPSender_main(arg):
+    print('MTPSender starting')
+    lowest_unACKed_seq_num = 0
 	# read the command line arguments
+    # recv_ip = sys.argv[1]             #127.0.0.1
+    # recv_port = sys.argv[2]
+    recv_port = 631
+    # wind_size = sys.argv[3]       #size of sliding window
+    wind_size = 5
+    # filename = sys.argv[4]        #filename = './1MB.txt'
+    filname = '/home/laura/Downloads/1MB.txt'
+    # log_filename = sys.argv[5]
 
 	# open log file and start logging
+    # logfile = open(log_filename, "a")
 
-	# open client socket and bind  - 'client opens up a UDP client socket'
+    # open client socket and bind  - 'client opens up a UDP client socket'
+    serverAddr = '192.168.1.14'
+    receiverAddr = '192.168.1.15'
+    serverPort = 49152
+    receiverPort = 49153
+    sendingSocket = socket(AF_INET, SOCK_DGRAM)  # AF_INET indicates that the underlying network is using IPv4
+        # SOCK_DGRAM indicates that socket is a UDP socket
+        # the OS creates the port number of the client socket (per textbook)
+    sendingSocket.settimeout(5)        #500ms timeout
+    receiverSocket = socket(AF_INET, SOCK_DGRAM)
+    receiverSocket.bind((receiverAddr,receiverPort))
+
+
 
 	# start receive thread
-	#recv_thread = threading.Thread(target=rec_thread,args=(client_socket,))
-	#recv_thread.start()
+	# recv_thread = threading.Thread(target=receive_thread,args=(clientSocket))
+	# recv_thread.start()
 
 
 	# take the input file and split it into packets (use create_packet)
@@ -72,23 +120,37 @@ def main():
     seqNum = 0
     # filename = sys.argv[4]
     input_file = open('/home/laura/Downloads/1MB.txt') #open arg4
-    # print(input_file)
+
+        #preprocess input_file
     str = ''
     for line in input_file:
         str += line
     bytes_arr = bytes(str, 'utf-8')
-    # print('bytes_arr: ', bytes_arr)
-    data = ''
-    data_size = 1444
+    print(sys.getsizeof(bytes_arr))
+    data_size = 1400
+    packets = []
+    lowest_unACKed_packet = 0
+    highest_unACKed_packet = 4
     while(len(bytes_arr)>0):
         data = bytes_arr[0:data_size]
         bytes_arr = bytes_arr[data_size:]
+        print(sys.getsizeof(bytes_arr))
         packet = create_packet(data,seqNum)
-        seqNum+=1
+
+        packets.append(packet)
+        # clientSocket.sendto(packet,serverName)
+        sendingSocket.sendto(packet,(serverAddr,serverPort))  # trial to ensure sockets can communicate
+        # print('sent packet: ', seqNum)
+        time.sleep(0.2)
+        seqNum += 1
+
 
 	# while there are packets to send:
 		# send packets to server using our unreliable_channel.send_packet()
 
+        # unreliable_channel.send_packet(clientSocket,packet,recv_port)    #last argument is an open port - 631
 		# update the window size, timer, etc.
 
+
+# MTPSender_main(0)
 
